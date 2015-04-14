04d9d8d1c6490b42be362772111aa6d6b895243b
Marc LaRocco <Cutmerock@gmail.com>
2015-04-13 23:31:48 -0400

    Displays number of days between two dates.
    This program will display the total number of days in between two dates.
    The user will select two days from the DateTimePicker.

---
+++ DaysGoneBy/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace DaysGoneBy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblDays = new System.Windows.Forms.Label();
+            this.dateStart = new System.Windows.Forms.DateTimePicker();
+            this.dateEnd = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Start Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ending Date";
+            // 
+            // lblDays
+            // 
+            this.lblDays.AutoSize = true;
+            this.lblDays.Location = new System.Drawing.Point(62, 175);
+            this.lblDays.Name = "lblDays";
+            this.lblDays.Size = new System.Drawing.Size(0, 17);
+            this.lblDays.TabIndex = 2;
+            // 
+            // dateStart
+            // 
+            this.dateStart.Location = new System.Drawing.Point(187, 34);
+            this.dateStart.Name = "dateStart";
+            this.dateStart.Size = new System.Drawing.Size(229, 22);
+            this.dateStart.TabIndex = 3;
+            this.dateStart.Value = new System.DateTime(2015, 4, 13, 0, 0, 0, 0);
+            // 
+            // dateEnd
+            // 
+            this.dateEnd.Location = new System.Drawing.Point(187, 109);
+            this.dateEnd.Name = "dateEnd";
+            this.dateEnd.Size = new System.Drawing.Size(229, 22);
+            this.dateEnd.TabIndex = 4;
+            this.dateEnd.Value = new System.DateTime(2015, 4, 13, 0, 0, 0, 0);
+            this.dateEnd.ValueChanged += new System.EventHandler(this.dateEnd_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 224);
+            this.Controls.Add(this.dateEnd);
+            this.Controls.Add(this.dateStart);
+            this.Controls.Add(this.lblDays);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Days Gone By";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblDays;
+        private System.Windows.Forms.DateTimePicker dateStart;
+        private System.Windows.Forms.DateTimePicker dateEnd;
+    }
+}
+
---
+++ DaysGoneBy/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DaysGoneBy
+{
+    public partial class Form1 : Form
+    {
+        double days;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void dateEnd_ValueChanged(object sender, EventArgs e)
+        {
+            //gives the variable the total value of days in between each date
+            days = (dateEnd.Value - dateStart.Value).TotalDays;
+            //convert the variable to an int
+            int newDay = Convert.ToInt32(days);
+            //display the total number of days between each date
+            lblDays.Text = "There are " + newDay.ToString() + " days between " + dateStart.Value.ToShortDateString() +" and " + dateEnd.Value.ToShortDateString() + ".";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
